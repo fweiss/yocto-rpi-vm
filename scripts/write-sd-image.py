df3012dc648eae80dd007502b6e0de72a36f4350
Frank E Weiss <feweiss@gmail.com>
2019-03-10 17:11:49 -0700

    Start to copy the boot files

--- scripts/write-sd-image.py
+++ scripts/write-sd-image.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import subprocess
+import shutil
 
 MACHINE = "raspberrypi0"
 PACKAGE = "core-image-base"
@@ -12,7 +13,8 @@ PACKAGE = "core-image-base"
 MOUNT_DIR = "/media/card"
 DEVICE_PATH = "/dev/sdf"
 
-BOOT_FILES_PATH = "./{}bcm2835-bootfiles".format(MACHINE)
+BOOT_FILES_PATH = "./{}/bcm2835-bootfiles".format(MACHINE)
+KERNEL_IMAGE_PATH = "./{}/zImage".format(MACHINE)
 ROOTFS_PATH = "./{}/{}-{}.tar.bz2".format(MACHINE, PACKAGE, MACHINE)
 
 def main():
@@ -20,10 +22,11 @@ def main():
         check_mount_dir()
         check_device_path()
         check_image_paths()
+        check_kernel_image_file()
         print("starting write of image {}".format(MACHINE))
         
-        on_mount(partition(1), format_null, [ copy_boot, rename_kernel_image ])
-        on_mount(partition(2), format_ext4, [ copy_rootfs ])
+        on_mount(partition(1), format_vfat, [ copy_boot_files, copy_kernel_image ])
+#        on_mount(partition(2), format_ext4, [ copy_rootfs ])
         
         print("finished write of image {}".format(MACHINE))
     except Exception as err:
@@ -48,6 +51,10 @@ def check_image_paths():
     if not os.path.exists(ROOTFS_PATH):
         raise Exception("rootfs tarball '{}' not found".format(ROOTFS_PATH))
 
+def check_kernel_image_file():
+    if not os.path.exists(KERNEL_IMAGE_PATH):
+        raise Exception("kernel image file '{}' not found".format(KERNEL_IMAGE_PATH))
+
 ############ filesytem helper functions
 
 def partition(partition):
@@ -58,6 +65,10 @@ def format_null(part):
 
 def format_vfat(part):
     print("vfat format for {}".format(part))
+    args = [ "sudo", "mkfs.vfat", "-F", "32", part, "-n", "BOOT" ]
+    cmd = subprocess.run(args, shell=False)
+    if cmd.returncode != 0:
+        raise Exception("could not format vfat on {}".format(part))
 
 def format_ext4(part):
     print("ext4 format for {}".format(part))
@@ -69,15 +80,31 @@ def format_ext4(part):
     
 ########### file copy functions
 
-def copy_boot():
+def copy_boot_files():
     # TODO
     #copy boot files (cmdline, config.txt)
     #copy boot partition
     #rename_kernel_img
-    print("copying boot")
+    print("copying boot loader files")
+#    args = [ "sudo", "cp", "{}/*".format(BOOT_FILES_PATH), MOUNT_DIR ]
+#    cmd = subprocess.run(args, shell=True)
+#    if cmd.returncode != 0:
+#        raise Exception("could not copy boot loader files")
+    for boot_file in os.listdir(BOOT_FILES_PATH):
+        full_file_name = os.path.join(BOOT_FILES_PATH, boot_file)
+        if os.path.isfile(full_file_name):
+#            shutil.copy(full_file_name, MOUNT_DIR)
+            args = [ "sudo", "cp", full_file_name, MOUNT_DIR ]
+            cmd = subprocess.run(args, shell=False)
+            if cmd.returncode != 0:
+                raise Exception("could not copy boot loader files")
 
-def rename_kernel_image():
-    print("renaming kernel image")
+def copy_kernel_image():
+    print("copying kernel image")
+    args = [ "sudo", "cp", KERNEL_IMAGE_PATH, "{}/kernel.img".format(MOUNT_DIR) ]
+    cmd = subprocess.run(args, shell=False)
+    if cmd.returncode != 0:
+        raise Ecception("could not copy kernel image")
 
 def copy_rootfs():
     # TODO copy (optibal) urandom, interfaces, wpa supplicant
